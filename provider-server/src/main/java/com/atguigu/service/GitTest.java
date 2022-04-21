e4d7a4c9ddd790fcd2ceef74e2aea39c4edef048
li <li@atguigu.com>
2022-04-21 15:08:01 +0800

    second commit

--- provider-server/src/main/java/com/atguigu/service/GitTest.java
+++ provider-server/src/main/java/com/atguigu/service/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.service;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("git test");
     }
 }
