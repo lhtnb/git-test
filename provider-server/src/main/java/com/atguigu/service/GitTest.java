b6cc734413adcd1fc16c6ef87dd49cd9b2f40e4c
li <li@atguigu.com>
2022-04-21 22:48:06 +0800

    master commit2

--- provider-server/src/main/java/com/atguigu/service/GitTest.java
+++ provider-server/src/main/java/com/atguigu/service/GitTest.java
@@ -3,6 +3,7 @@ package com.atguigu.service;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
+        System.out.println("hello git");
         System.out.println("git hello");
         System.out.println("master hello");
         System.out.println("hot-fix test");
